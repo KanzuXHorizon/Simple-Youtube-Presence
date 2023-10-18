ca90f42ffcf101cb244a09a541c326d8f23994f9
KanzuWakazaki <KanzuWakazaki.Github@protonmail.com>
2023-10-18 18:21:15 +0700

    Update index.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -26,19 +26,40 @@ setInterval(async function() {
         const timenow = document.querySelector('.ytp-time-duration').textContent;
         const thumbnail = video_obj.thumbnailUrl[0];//`https://i3.ytimg.com/vi/${location.href.split('=')[1].split('&')[0]}/maxresdefault.jpg`;
         const author = video_obj.author;
-        const author_img = document.querySelectorAll('.style-scope .ytd-video-owner-renderer .no-transition')[0].childNodes[2].src.split('=')[0];
         try {
-            console.log(video_obj,video_duration , timenow , author_img, thumbnail , author);
-        }
-        catch (e) { console.log(e) }
 
-        if (video_obj.name && video_duration && timenow && author_img && thumbnail && author) {
-            await fetch(`http://localhost:4400/?author=${encodeURIComponent(author)}&thumbnail=${encodeURIComponent(thumbnail)}&img=${encodeURIComponent(author_img)}&url=${encodeURIComponent(video_url)}&title=${encodeURIComponent(video_obj.name)}&time=${video_duration}&end=${timenow}`)
-        }
-        else {
-            inyoureye = ''
-            console.log('số')
-        }
+            let author_img = '';
+            const normal = ytInitialData.contents.twoColumnWatchNextResults.results.results.contents[1].videoSecondaryInfoRenderer.owner.videoOwnerRenderer.thumbnail.thumbnails[2].url;
+            const dt = ytInitialData.contents.twoColumnWatchNextResults.secondaryResults.secondaryResults.results;
+                for (let i of dt) {
+                    if (i.compactVideoRenderer != undefined && i.compactVideoRenderer.videoId != undefined) {
+                        if (i.compactVideoRenderer.videoId == (video_obj.thumbnailUrl[0]).split('/')[4]) {
+                            author_img = i.thumbnail != undefined ? i.thumbnail.thumbnails : i.compactVideoRenderer.channelThumbnail.thumbnails[0].url
+                            break;
+                        }
+                    }
+                }
+                console.log(author_img || "empty athr");
+            if (author_img == undefined || author_img == '') {
+                if (document.querySelector("#owner > ytd-video-owner-renderer > a") != undefined && document.querySelector("#owner > ytd-video-owner-renderer > a").childNodes[0] != undefined && document.querySelector("#owner > ytd-video-owner-renderer > a").childNodes[0].childNodes[2] != undefined && document.querySelector("#owner > ytd-video-owner-renderer > a").childNodes[0].childNodes[2].src) {
+                    author_img = document.querySelector("#owner > ytd-video-owner-renderer > a").childNodes[0].childNodes[2].src;
+                }
+                else if (ytInitialPlayerResponse.endscreen != undefined && ytInitialPlayerResponse.endscreen.endscreenRenderer.elements[0].endscreenElementRenderer.image.thumbnails[0].url != undefined) {
+                    author_img = ytInitialPlayerResponse.endscreen.endscreenRenderer.elements[0].endscreenElementRenderer.image.thumbnails[0].url;
+                }
+                else author_img = normal;
+            }
+
+            console.log(video_obj,video_duration , video_duration , author_img, thumbnail , author);
+            if (video_obj.name && video_duration && video_duration && author_img && thumbnail && author) {
+                await fetch(`http://localhost:4400/?author=${encodeURIComponent(author)}&thumbnail=${encodeURIComponent(thumbnail)}&img=${encodeURIComponent(author_img)}&url=${encodeURIComponent(video_url)}&title=${encodeURIComponent(video_obj.name)}&time=${video_duration}&end=${timenow}`)
+            }
+            else {
+                inyoureye = ''
+                console.log('số')
+            }
+       }
+        catch (e) { console.log(e) }
     }
     else console.log(112)
 },500)
