bb85175a48e1820e8384e9e6ec023a48ba873db2
KanzuWakazaki <KanzuWakazaki.Github@protonmail.com>
2023-10-19 13:53:45 +0700

    Fix time countdown when ytb appear ads

--- Javascript/index.js
+++ Javascript/index.js
@@ -13,7 +13,7 @@
     'use strict';
 
     var inyoureye = ''
-//document.querySelectorAll(".ytp-play-button")[0].title
+//document.querySelectorAll(".ytp-play-button")[0].title *pause *resume
 setInterval(async function() {
     if (location.href == 'https://www.youtube.com/watch?v=xjOHEjINQUQ') { return; }
     const video_obj = JSON.parse(document.querySelectorAll('#scriptTag')[0].innerHTML);
@@ -21,44 +21,69 @@ setInterval(async function() {
     if (inyoureye == video_obj.name) { return; }
     if (inyoureye == '' || !video_obj.name == inyoureye || inyoureye.length != video_obj.name.length) {
         inyoureye = video_obj.name;
-        const video_duration = document.querySelector('.ytp-time-current').textContent;
-        const video_url = location.href;
-        const timenow = document.querySelector('.ytp-time-duration').textContent;
-        const thumbnail = video_obj.thumbnailUrl[0];//`https://i3.ytimg.com/vi/${location.href.split('=')[1].split('&')[0]}/maxresdefault.jpg`;
-        const author = video_obj.author;
         try {
-
-            let author_img = '';
-            const normal = ytInitialData.contents.twoColumnWatchNextResults.results.results.contents[1].videoSecondaryInfoRenderer.owner.videoOwnerRenderer.thumbnail.thumbnails[2].url;
-            const dt = ytInitialData.contents.twoColumnWatchNextResults.secondaryResults.secondaryResults.results;
-                for (let i of dt) {
-                    if (i.compactVideoRenderer != undefined && i.compactVideoRenderer.videoId != undefined) {
-                        if (i.compactVideoRenderer.videoId == (video_obj.thumbnailUrl[0]).split('/')[4]) {
-                            author_img = i.thumbnail != undefined ? i.thumbnail.thumbnails : i.compactVideoRenderer.channelThumbnail.thumbnails[0].url
-                            break;
+            function getData() {
+                const vid_obj = JSON.parse(document.querySelectorAll('#scriptTag')[0].innerHTML);
+                const video_duration = document.querySelector('.ytp-time-current').textContent;
+                const video_url = location.href;
+                const timenow = document.querySelector('.ytp-time-duration').textContent;
+                const thumbnail = video_obj.thumbnailUrl[0];//`https://i3.ytimg.com/vi/${location.href.split('=')[1].split('&')[0]}/maxresdefault.jpg`;
+                const author = video_obj.author;
+                let author_img = '';
+                const normal = ytInitialData.contents.twoColumnWatchNextResults.results.results.contents[1].videoSecondaryInfoRenderer.owner.videoOwnerRenderer.thumbnail.thumbnails[2].url;
+                const dt = ytInitialData.contents.twoColumnWatchNextResults.secondaryResults.secondaryResults.results;
+                    for (let i of dt) {
+                        if (i.compactVideoRenderer != undefined && i.compactVideoRenderer.videoId != undefined) {
+                            if (i.compactVideoRenderer.videoId == (video_obj.thumbnailUrl[0]).split('/')[4]) {
+                                author_img = i.thumbnail != undefined ? i.thumbnail.thumbnails : i.compactVideoRenderer.channelThumbnail.thumbnails[0].url
+                                break;
+                            }
                         }
                     }
+                    console.log(author_img || "empty athr");
+                if (author_img == undefined || author_img == '') {
+                    if (document.querySelector("#owner > ytd-video-owner-renderer > a") != undefined && document.querySelector("#owner > ytd-video-owner-renderer > a").childNodes[0] != undefined && document.querySelector("#owner > ytd-video-owner-renderer > a").childNodes[0].childNodes[2] != undefined && document.querySelector("#owner > ytd-video-owner-renderer > a").childNodes[0].childNodes[2].src) {
+                        author_img = document.querySelector("#owner > ytd-video-owner-renderer > a").childNodes[0].childNodes[2].src;
+                    }
+                    else if (ytInitialPlayerResponse.endscreen != undefined && ytInitialPlayerResponse.endscreen.endscreenRenderer.elements[0].endscreenElementRenderer.image.thumbnails[0].url != undefined) {
+                        author_img = ytInitialPlayerResponse.endscreen.endscreenRenderer.elements[0].endscreenElementRenderer.image.thumbnails[0].url;
+                    }
+                    else author_img = normal;
                 }
-                console.log(author_img || "empty athr");
-            if (author_img == undefined || author_img == '') {
-                if (document.querySelector("#owner > ytd-video-owner-renderer > a") != undefined && document.querySelector("#owner > ytd-video-owner-renderer > a").childNodes[0] != undefined && document.querySelector("#owner > ytd-video-owner-renderer > a").childNodes[0].childNodes[2] != undefined && document.querySelector("#owner > ytd-video-owner-renderer > a").childNodes[0].childNodes[2].src) {
-                    author_img = document.querySelector("#owner > ytd-video-owner-renderer > a").childNodes[0].childNodes[2].src;
+    
+                //console.log(video_obj,video_duration , video_duration , author_img, thumbnail , author);
+                return {
+                    vid_obj,video_duration,video_url,timenow,thumbnail,author,author_img
                 }
-                else if (ytInitialPlayerResponse.endscreen != undefined && ytInitialPlayerResponse.endscreen.endscreenRenderer.elements[0].endscreenElementRenderer.image.thumbnails[0].url != undefined) {
-                    author_img = ytInitialPlayerResponse.endscreen.endscreenRenderer.elements[0].endscreenElementRenderer.image.thumbnails[0].url;
-                }
-                else author_img = normal;
             }
 
-            console.log(video_obj,video_duration , video_duration , author_img, thumbnail , author);
-            if (video_obj.name && video_duration && video_duration && author_img && thumbnail && author) {
-                await fetch(`http://localhost:4400/?author=${encodeURIComponent(author)}&thumbnail=${encodeURIComponent(thumbnail)}&img=${encodeURIComponent(author_img)}&url=${encodeURIComponent(video_url)}&title=${encodeURIComponent(video_obj.name)}&time=${video_duration}&end=${timenow}`)
+            const { vid_obj, video_duration,video_url,timenow,thumbnail,author,author_img } = getData();
+
+
+            if (vid_obj.name && video_duration && video_duration && author_img && thumbnail && author) {
+                async function send(Author, Thumbnail, Image, Url, Title, Now, Duration) {
+                    await fetch(`http://localhost:4400/?author=${encodeURIComponent(Author)}&thumbnail=${encodeURIComponent(Thumbnail)}&img=${encodeURIComponent(Image)}&url=${encodeURIComponent(Url)}&title=${encodeURIComponent(Title)}&time=${Duration}&end=${Now}`)
+                }
+                await send(author,thumbnail,author_img,video_url,vid_obj.name,timenow,video_duration);
+                if (document.querySelector("#simple-ad-badge\\:8") != undefined && document.querySelector("#simple-ad-badge\\:8").innerHTML.includes('tài trợ')) {
+                    global.checkAdInterval = setInterval(async function() {
+                        if (document.querySelector("#simple-ad-badge\\:8") == undefined) {
+                            clearInterval(global.checkAdInterval)
+                            await new Promise((res, rej) => setTimeout(res,1000));
+                            const { vid_obj:a, video_duration:b,video_url:c,timenow:d,thumbnail:e,author:f, author_img:g } = getData();
+                            await send(f,e,g,c,a.name,d,b);
+                        }
+                        else {
+                            return;
+                        }
+                    },  500)
+                }
             }
             else {
                 inyoureye = ''
                 console.log('số')
             }
-       }
+        }
         catch (e) { console.log(e) }
     }
     else console.log(112)
@@ -66,3 +91,4 @@ setInterval(async function() {
 
 
 })();
+//pause - resume 
